13e64e5b120026eb2a4708cbeb8adc395a16b978
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2011-11-25 18:25:38 -0600

    Added SwapLikes() feature

--- j/script.js
+++ j/script.js
@@ -1,3 +1,28 @@
 $('#app-preview, .popup').click(function() {
   $('.popup').toggleClass('visible')
 });
+
+window.fbAsyncInit = function() 
+{
+	FB.Event.subscribe('edge.create',
+		function(response)
+		{
+			SwapLikes(response);
+		}
+	);
+};
+		
+function SwapLikes(likedUrl)
+{
+	if(likedUrl == 'http://neosmart.net/EasyBCD/')
+	{
+		$('#fb1').css('display', 'none');
+		$('#fb2').css('display', '');
+	}
+}
+
+$(document).ready(function()
+	{
+		$('#fb2').css('display', 'none');
+	}
+);
