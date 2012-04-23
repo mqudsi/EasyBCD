331b41ff47d3f3c1862414c79c88cc8046345183
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2012-04-23 09:32:49 +0300

    Google Analytics

--- index.php
+++ index.php
@@ -29,6 +29,20 @@ if($_SERVER["REQUEST_URI"] != "/EasyBCD/")
 			$.getScript("//platform.twitter.com/widgets.js");
 		});
 	</script>
+	<script type="text/javascript">	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-277958-1']);
+	  _gaq.push(['_setDomainName', 'neosmart.net']);
+	  _gaq.push(['_setAllowLinker', true]);
+	  _gaq.push(['_setAllowHash', 'false']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();	
+	</script>
 
 </head>
 
