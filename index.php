d855626362ab5856d27daab19abbd8bc3f139b80
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2012-04-24 10:14:58 +0300

    Added async flag to fbjs

--- index.php
+++ index.php
@@ -228,10 +228,13 @@ s.parentNode.insertBefore(po, s);
 </script>
 
 <div id="fb-root"></div>
-<script type="text/javascript">(function(d, s, id) {
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
- fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script type="text/javascript">
+	(function(d, s, id) {
+	 var js, fjs = d.getElementsByTagName(s)[0];
+	 if (d.getElementById(id)) {return;}
+	 js = d.createElement(s); js.id = id;
+	 js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+	 js.async = true;
+	 fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
