7369b71ce762df70eba0ac18ada8ef728400beb8
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2012-10-22 10:49:17 +0300

    Using new registration link

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ if($_SERVER["REQUEST_URI"] != "/EasyBCD/")
 							<li><strong>No</strong> early access to new features
 							<li>Only for <strong>personal, non-commercial</strong> use
 						</ul>
-						<a class="cta-button" href="http://neosmart.net/download.php?id=1">Register</a>
+						<a class="cta-button" href="http://neosmart.net/Download/Register/1">Register</a>
 					</div>
 				<li class="commercial solution">
 					<div class="data">
